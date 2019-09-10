e95763596d7708ee767bae6e034bdb667c1aaf71
unknown <tromavoto@abv.bg>
2019-09-10 16:09:04 +0300

    Generated default Asp.Net Core 2.2 MVC project.

---
+++ src/GamingForum/GamingForum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GamingForum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
